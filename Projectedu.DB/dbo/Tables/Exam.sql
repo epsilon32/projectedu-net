7c16cec1632fe7a2c067c7781438d1ec6a8ecf34
Christopher Maister <christopher.maister@gmail.com>
2021-05-04 19:44:51 +0900

    Recreating all files

--- Projectedu.DB/dbo/Tables/Exam.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Exam]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [CreatedBy] INT NOT NULL, 
-    [CreatedDate] DATETIME2 NOT NULL DEFAULT getutcdate(), 
-    [LastModified] DATETIME2 NOT NULL DEFAULT getutcdate(),
-    [Description] NVARCHAR(MAX) NULL
-)
